48188e475641f4fdc998d0db6a9f9078d19b8604
vinothgithuglogin <vinoth.natarajan6343@gmail.com>
2021-02-12 03:11:22 +0530

    success

---
+++ coreproject/src/main/java/selenium/core/coreproject/App.java
@@ -0,0 +1,13 @@
+package selenium.core.coreproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello zealous" );
+    }
+}
